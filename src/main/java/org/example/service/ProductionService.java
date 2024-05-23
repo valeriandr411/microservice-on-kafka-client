0b79b9ed6ee728cb5f638912d4edc0543bf35162
PokhilenkoVV <pokhilenko@lanit.ru>
2024-05-24 00:08:21 +0300

    добавлена зависимость для rest-assured

--- src/main/java/org/example/service/ProductionService.java
+++ src/main/java/org/example/service/ProductionService.java
@@ -8,7 +8,6 @@ import org.example.kafka.KafkaProducerConfig;
 import org.example.model.Order;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Service
