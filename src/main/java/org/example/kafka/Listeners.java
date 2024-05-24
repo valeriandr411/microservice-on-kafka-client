effa51039fffac032db1217ce366b79904a8b7e7
PokhilenkoVV <pokhilenko@lanit.ru>
2024-05-25 00:00:44 +0300

    статус заказа теперь меняется на стороне клиента

--- src/main/java/org/example/kafka/Listeners.java
+++ src/main/java/org/example/kafka/Listeners.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.example.model.Order;
+import org.example.service.ClientService;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +14,10 @@ import org.springframework.stereotype.Service;
 public class Listeners {
     private final ObjectMapper objectMapper = new ObjectMapper();
     @KafkaListener(topics = "completed", groupId = "app123")
-    public void msListener(ConsumerRecord<Long,Order> record) {
+    public void msListener(ConsumerRecord<Long,Order> record) throws JsonProcessingException {
+        String order = String.valueOf(record.value());
+        Order order2 = objectMapper.readValue(order,Order.class);
+        ClientService.getOrder(order2.getId()).setStatus("COMPLETED");
         System.out.println("Заказ готов: " + record.value());
         System.out.println("----------------------------------------");
     }
